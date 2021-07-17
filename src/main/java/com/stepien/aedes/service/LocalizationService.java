d8bb9d0f8da34232c501fbdb362bbaffeda6bb42
Gabriel Stepien <gabriel.stepien@inf.ufrgs.br>
2021-07-17 14:49:50 -0300

    Added a comment in get chunks

--- src/main/java/com/stepien/aedes/service/LocalizationService.java
+++ src/main/java/com/stepien/aedes/service/LocalizationService.java
@@ -8,5 +8,11 @@ import com.stepien.aedes.vo.GridPosition;
 
 public interface LocalizationService {
     GridPosition getGridPosition(GeoPoint geoPoint);
+    
+    /**
+     * Returns all the chunks that are in the system, this way we can display than in the frontend
+     * @param geoPoint
+     * @return
+     */
     List<Chunks> getAllChunks();
 }
